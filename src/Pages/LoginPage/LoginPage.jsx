19fda109a94ec2ecfb3f22d06ae1189b44006d67
Aryan1982 <ap1982o3@gmail.com>
2022-12-10 14:47:50 +0530

    login-page-updated

--- src/Pages/LoginPage/LoginPage.jsx
+++ src/Pages/LoginPage/LoginPage.jsx
@@ -1,14 +1,26 @@
 import React from 'react'
 import "./LoginPage.css"
+import GoogleIcon from "./google.svg"
 
 const LoginPage = () => {
   return (
     <div className="LoginPageBody">
       <div className="LoginPageCard">
-      <div className="logindiv">Login</div>
-      <div className="hexagon"></div>
-      <div className="togglebutton"></div>
+        <div className="signindiv">SIGN-IN</div>
+        <div className="emailinput">
+          <h1>EMAIL-ID</h1>
+          <input type="text"></input>
+        </div>
+        <button className="requestOTPbtn">REQUEST OTP</button>
+        <div className="signUpOptionDiv">
+          <div className='orsignupwith'><h2>or sign up with</h2><div className="cardFooter">
+          <img src={GoogleIcon}/><h1>Google</h1>
+          </div>
+          </div>
+          
+        </div>
       </div>
+
     </div>
   )
 }
